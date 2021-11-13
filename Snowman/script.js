d6ff0abf44fb1bdd37de6b4bc876ab3156803b44
Bel1la <92915421+Bel1la@users.noreply.github.com>
2021-11-13 19:21:39 +0500

    add snowman

---
+++ Snowman/script.js
@@ -0,0 +1,29 @@
+let x=0, y=0, dragItem="";
+document.body.addEventListener('dragstart',moveStart);
+document.body.addEventListener('dragover',moveDragOver);
+document.body.addEventListener('drop',moveDrop);
+
+
+
+function moveStart(e){
+    dragItem=e.target;
+    x=e.offsetX;
+    y=e.offsetY;
+
+    dragItem.style.zIndex=10;
+}
+
+function moveDrop(e){
+    e.preventDefault();
+    
+    dragItem.style.left =(e.pageX-x) + "px";
+    dragItem.style.top =(e.pageY-y) + "px";
+    
+}
+function moveDragOver(e){
+    e.preventDefault();
+    
+}
+function resetZIndex(){
+    dragItem.style.zIndex=5;
+}
